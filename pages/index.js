5e241d6b563d2be1b1813cdf824cfca142f6973c
Luciano Custodio da Silva Junior <luciano.silva380@gmail.com>
2021-05-19 18:57:12 -0300

    fixing manifest

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,12 @@ export default function Index() {
       <meta name='description' content='Description' />
       <meta name='keywords' content='Keywords' />
       <title>Star Wars Starships</title>
-
+      <link rel="icon" href="public/icons/favicon.ico" type="image/x-icon"></link>
       <link rel='manifest' href='/manifest.json' />
-      <link href='/public/icons/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
-      <link href='/public/icons/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
-      <link href='/public/icons/favicon-48x48.png' rel='icon' type='image/png' sizes='48x48' />
-      <link rel='/public/icons/apple-icon' href='/apple-icon.png'></link>
+      <link href='icons/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+      <link href='icons/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+      <link href='icons/favicon-48x48.png' rel='icon' type='image/png' sizes='48x48' />
+      <link rel='icons/apple-icon' href='/apple-icon.png'></link>
       <meta name='theme-color' content='#317EFB' />
     </Head>
       <Header />
